37a9ea112b18146958c73bbb0f183cb97be8e802
MikhailVTs <109409779+MikhailVTs@users.noreply.github.com>
2023-06-06 10:28:55 +1000

    Update Communication.java
    Редактирование кода Communication

--- task_1/src/Communication.java
+++ task_1/src/Communication.java
@@ -1,10 +1,30 @@
 
 public class Communication extends People {
 
+    private TypeGender sex;
+    private TypeCommunication kinship;
+
     public Communication(String lastname, String firstname, TypeGender sex, int yearOfBirth,
             TypeCommunication kinship) {
-        super(lastname, firstname, sex, yearOfBirth, kinship);
-        // TODO Auto-generated constructor stub
+
+        super(lastname, firstname, yearOfBirth);
+
+        this.sex = sex;
+        this.kinship = kinship;
+
+    }
+
+    public TypeGender getSex() {
+        return sex;
+    }
+
+    public TypeCommunication getKinship() {
+        return kinship;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + ", пол: " + sex + ", родство: " + kinship;
     }
 
 }
