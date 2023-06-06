6d4ac556fbe9abbe46bca4676975df7d9a64c77b
MikhailVTs <109409779+MikhailVTs@users.noreply.github.com>
2023-06-06 10:30:20 +1000

    Update Main.java
    Редактирование кода Main

--- task_1/src/Main.java
+++ task_1/src/Main.java
@@ -4,14 +4,15 @@
 public class Main {
 
     public static void main(String[] args) {
-        
+
         FamilyTree familytree = new FamilyTree();
-        People im = new People("Иванов", "Иван", TypeGender.Female, 2016, TypeCommunication.Im);
-        People dad = new Communication("Иванов", "Пётр", TypeGender.Female, 1990, TypeCommunication.Dad);
-        People mom = new Communication("Иванова", "Лариса", TypeGender.Male, 1990, TypeCommunication.Mom);
-        People grandpa = new Communication("Иванов", "Сергей", TypeGender.Female, 1970, TypeCommunication.Grandpa);
+
+        People im = new People("Иванов", "Иван", 2010);
+        People dad = new Communication("Иванов", "Пётр", TypeGender.Male, 1990, TypeCommunication.Dad);
+        People mom = new Communication("Иванова", "Лариса", TypeGender.Female, 1990, TypeCommunication.Mom);
+        People grandpa = new Communication("Иванов", "Сергей", TypeGender.Male, 1970, TypeCommunication.Grandpa);
         People grandma = new Communication("Иванова", "Раиса", TypeGender.Female, 1970, TypeCommunication.Grandmother);
-        
+
         familytree.addPeople(im);
         familytree.addPeople(dad);
         familytree.addPeople(mom);
